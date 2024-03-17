716857dab51f7aaebe253eed74b57979f5ea49ed
Аліна Кононова <alinakononovaaa@gmail.com>
2024-03-17 23:26:42 +0200

    elements visibillity

--- cypress/e2e/wikiTest.cy.js
+++ cypress/e2e/wikiTest.cy.js
@@ -2,9 +2,13 @@ describe('Test in Cypress', () => {
   it('Open page for English in Wikipediia', () => {
     cy.visit('https://www.wikipedia.org/');
     cy.get('a#js-link-box-en').click();
-    
+
     cy.location().should((loc) =>{
       expect(loc.href).to.eq('https://en.wikipedia.org/wiki/Main_Page');
     })
+
+    cy.get('li#pt-createaccount-2').should('be.visible');
+    cy.get('li#pt-login-2').should('be.visible');
+    cy.get('div#vector-user-links-dropdown').should('be.visible');
   })
 })
